41a44d6026d3f0552872d82f7603d965f154cb35
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-26 18:50:25 +0100

    class 'Hase'

---
+++ src/main/java/at/campus02/putz/hase/Hase.java
@@ -0,0 +1,26 @@
+package at.campus02.putz.hase;
+
+public class Hase {
+
+    private String name;
+
+    public Hase(String name) {
+        this.name = name;
+    }
+
+    public void schlafen(){
+        System.out.println(this.name + " schläft.");
+    }
+
+    public void hoppeln(){
+        System.out.println(this.name + " hoppelt.");
+    }
+
+    public void fressen(){
+        System.out.println(this.name + " frisst.");
+    }
+
+    public void verteilen(){
+        System.out.println(this.name + " ist ein Hase und verteilt nichts.");
+    }
+}
