d4b08b2fa95af71ca58f10ce20e20a81cdfeb9ce
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-26 16:12:20 +0100

    Demo Class

---
+++ src/main/java/Demo.java
@@ -0,0 +1,17 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Customer Putzi = new Customer(1);
+        Customer Leo = new Customer(2);
+
+        Putzi.addAddress(new Address("Weizerstrasse 1", "8010", "Graz", "Austria"));
+        Leo.addAddress(new Address("Bachgasse 7a", "8020", "Graz", "Austria"));
+        Putzi.addAddress(new Address("Siebengasse 8a", "1010", "Wien", "Austria"));
+
+        System.out.println("Putzi: " + Putzi.toString());
+        System.out.println("Leo: " + Leo.toString());
+
+    }
+
+}
