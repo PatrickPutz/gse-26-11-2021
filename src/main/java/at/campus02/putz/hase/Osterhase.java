829289444278c245acce44dfbda17b0ba18fe1d6
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-26 18:55:32 +0100

    class 'Osterhase'

---
+++ src/main/java/at/campus02/putz/hase/Osterhase.java
@@ -0,0 +1,13 @@
+package at.campus02.putz.hase;
+
+public class Osterhase extends Hase{
+
+    public Osterhase(String name) {
+        super(name);
+    }
+
+    @Override
+    public void verteilen() {
+        System.out.println(this.name + " versteckt Geschenke und Ostereier.");
+    }
+}
