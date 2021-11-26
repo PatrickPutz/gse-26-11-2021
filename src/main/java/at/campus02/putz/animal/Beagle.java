efa23e9467257dfbef0d35f60653452fa1709b1d
PatrickPutz <Putz.Patrick@outlook.com>
2021-11-26 17:10:33 +0100

    Beagle Class & Demo App

--- src/main/java/at/campus02/putz/animal/Beagle.java
+++ src/main/java/at/campus02/putz/animal/Beagle.java
@@ -2,6 +2,18 @@ package at.campus02.putz.animal;
 
 public class Beagle extends Dog {
 
+    public String lovedFood;
 
+    public void doSomething2(){
+        System.out.println("This is a beagle!");
+    }
 
+    @Override
+    public String toString() {
+        return "Beagle{" +
+                "lovedFood='" + lovedFood + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
--- src/main/java/at/campus02/putz/animal/DemoDogApp.java
+++ src/main/java/at/campus02/putz/animal/DemoDogApp.java
@@ -3,7 +3,21 @@ package at.campus02.putz.animal;
 public class DemoDogApp {
     public static void main(String[] args) {
 
+        Dog d = new Dog();
+        d.eyeColor = "brown";
+        d.weight = 2900;
 
+        System.out.println(d.toString());
+        d.doSomething();
+
+        Beagle b = new Beagle();
+        b.eyeColor = "blue";
+        b.weight = 2850;
+        b.lovedFood = "burger";
+
+        System.out.println(b.toString());
+        b.doSomething();
+        b.doSomething2();
 
     }
 }
